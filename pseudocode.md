fab815f1f945280e14daab15eb9986f3c59cc5c8
cavazosdotcom <cavazosdotcom@gmail.com>
2021-11-16 23:50:14 -0800

    added arrays with characters

---
+++ pseudocode.md
@@ -0,0 +1,36 @@
+# What does my application need to function
+
+- List for all needed numbers (0-9)
+- List for all lowercase letter (a-z)
+- List for all uppercase letter (A-Z)
+- List for all special characters (!-*)
+
+# What does my application need to get from the user
+
+For each of the following, `Declare` a variable to store the value.
+
+- Password length - verifdy that it's between 8 and 128
+- Should numbers be included
+- Should lowercase letters be included
+- Should uppercase letters be included
+- Should special characters be included 
+
+# What steps are needed to build a password
+
+- ASking the user for the criteria
+
+- Make list that onlt has the type of characters matching the requested options
+
+- Randomly select a character from my list
+
+- Add our randomly selected character to our password
+
+- Repeat until the password reaches the requested length
+
+# Pseudocode
+
+- numbers list ['1','2','3','4','5'...]
+- upper list ['A','B','C','D'...]
+- lower list ['a','b','c','d'...]
+- special list ['!','@','#','$'...]
+
---
+++ script.js
@@ -0,0 +1,72 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var special = ["!","@","#","$","%","^","&","*","(",")","-","_","~","`","=","+"];
+
+// Data = array, strings, numbers, booleans
+
+//  Modify Data = Reassigning a new value to an existing variable
+
+//  Create new data
+
+//  Gather data from the user
+
+//  Modify Existing data
+
+//  Validate data (comparison operators IF/ELSE 
+
+
+// alert()
+// prompt()
+// confirm() 
+
+
+var myString = "Chris";
+
+myString = 1;
+
+myString += 1;
+
+
+function generatePassword (){
+
+  var password = "";
+
+  // console.log('generate a apassword.')
+
+  // loop to repeat, selecting a random character and then appending it to the string
+  // loop until our string reaches chosen length
+
+  
+
+  password == "a";
+
+  console.log(password);
+
+  password += "r";
+
+  console.log(password);
+
+  password += "5";
+
+  console.log(password);
+
+  password += "]";
+
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
